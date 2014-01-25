c55da2dad44c1f1843f48cd00b9a0ddc54dcf3bd
Victor Ivanov <vic.ivanoff@gmail.com>
2014-01-25 19:54:55 +0700

    counting keyword suggestions

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -8,4 +8,22 @@ class Keyword < ActiveRecord::Base
     suggestions.each { |s| result += s.variants.count }
     result
   end
+
+  def suggestions_count
+    result = {}
+    seed_keyword = self.value.split(' ')
+    suggestions.each do |s|
+      s.variants.each do |v|
+        keywords = v.split(' ') - seed_keyword
+        keywords.each do |k|
+          if result[k]
+            result[k] = result[k] + 1
+          else
+            result[k] = 1
+          end
+        end
+      end
+    end
+    Hash[result.sort_by {|k, v| -v}]
+  end
 end
--- test/models/keyword_test.rb
+++ test/models/keyword_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class KeywordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "suggestions_count" do
+     k = Keyword.new(:value => 'ipad cover')
+     s = Suggestion.new(:variants => ['ipad cover black', 'ipad cover white'])
+     s2 = Suggestion.new(:variants => ['ipad cover yellow', 'ipad cover white with stand'])
+     k.suggestions << [s, s2]
+     suggestions_count = k.suggestions_count
+     assert suggestions_count['black'] == 1
+     assert suggestions_count['white'] == 2
+     #assert suggestions_count.first[0] == 'white'
+     assert !suggestions_count['ipad']
+     puts suggestions_count.first[0]
+     puts suggestions_count.first[1]
+     puts suggestions_count
+   end
 end
